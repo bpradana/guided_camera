1c1102e83314d38bcab6c10e3aa15da6133f10f6
Bintang Pradana Erlangga Putra <bintangpradana02@gmail.com>
2022-08-25 15:47:48 +0700

    add crop

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 import 'package:guided_camera/screens/camera.dart';
+import 'package:image/image.dart' as imglib;
+// import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,7 +14,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   dynamic imagePath;
-  dynamic faceDetect;
+  // dynamic faceDetect;
 
   @override
   Widget build(BuildContext context) {
@@ -24,17 +27,17 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              (faceDetect != null) ? 'Face Detected' : 'No Face Detected',
-              style: const TextStyle(fontSize: 20),
-            ),
+            // Text(
+            //   (faceDetect != null) ? 'Face Detected' : 'No Face Detected',
+            //   style: const TextStyle(fontSize: 20),
+            // ),
             Container(
               margin: const EdgeInsets.only(
                 bottom: 30,
               ),
               color: Colors.grey,
               height: MediaQuery.of(context).size.height * 0.5,
-              width: MediaQuery.of(context).size.width * 0.5,
+              width: MediaQuery.of(context).size.height * 0.5,
               child: (imagePath != null)
                   ? Image.network(imagePath)
                   : const SizedBox(),
@@ -45,8 +48,13 @@ class _HomeScreenState extends State<HomeScreen> {
                     context,
                     MaterialPageRoute(
                         builder: (builder) => const CameraScreen()));
-                faceDetect = await FaceDetector(options: FaceDetectorOptions())
-                    .processImage(InputImage.fromFilePath(imagePath));
+                var bytes = await File(imagePath).readAsBytes();
+                imglib.Image? src = imglib.decodeImage(bytes);
+                imglib.Image destImage = imglib.copyCrop(src!, 0, 0, 100, 100);
+                var jpg = imglib.encodeJpg(destImage);
+                await File(imagePath).writeAsBytes(jpg);
+                // faceDetect = await FaceDetector(options: FaceDetectorOptions())
+                //     .processImage(InputImage.fromFilePath(imagePath));
                 setState(() {
                   imagePath;
                 });
